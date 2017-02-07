057e00ac43cd0eb89d69d757752aa31937c1e52a
s.torrellas <s.torrellas@whisbi.com>
2017-02-07 15:28:32 +0000

    ENH: Added another endpoint

--- src/main/java/com/whisbi/kpi/WhisbikpispringApplication.java
+++ src/main/java/com/whisbi/kpi/WhisbikpispringApplication.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.whisbi.kpi.InteractionKpi.interaction_type;
+import com.whisbi.kpi.InteractionKpi.room_type;
+import com.whisbi.kpi.InteractionKpi.room_user_type;
+
 @RestController
 @SpringBootApplication
 public class WhisbikpispringApplication {
@@ -29,7 +33,7 @@ public class WhisbikpispringApplication {
     }
 
     @RequestMapping("/api/tracking")
-    public Greeting interaction(@RequestParam String branch, 
+    public ClientKpi tracking(@RequestParam String branch, 
     					        @RequestParam String clientbrowser, 
     					        @RequestParam String clientdevice,
     					        @RequestParam String clientos,
@@ -40,13 +44,29 @@ public class WhisbikpispringApplication {
     					        ) {
     	
     	log.info("Request URL: " + request.getRequestURL().toString() + " " + request.getQueryString());
-    	new ClientKpi(branch, clientbrowser, clientdevice, clientos, cookieguid, kpiguid, kpi);
-    	
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, "sergi"));
+    	return new ClientKpi(branch, clientbrowser, clientdevice, clientos, cookieguid, kpiguid, kpi);
+    }
+    
+
+    @RequestMapping("/api/interaction")
+    public InteractionKpi interaction(@RequestParam int interaction_code, 
+    					        @RequestParam String interaction_value, 
+    					        @RequestParam String session_guid,
+    					        @RequestParam int user_id,
+    					        @RequestParam int user_type,
+    					        @RequestParam int room_type,
+    					        HttpServletRequest request
+    					        ) {    	
+    	log.info("Request URL: " + request.getRequestURL().toString() + "?" + request.getQueryString());
+    	    	
+    	InteractionKpi.interaction_type interaction_type_enum = InteractionKpi.interaction_type.values()[interaction_code];
+    	InteractionKpi.room_user_type user_type_enum = InteractionKpi.room_user_type.values()[user_type];
+    	InteractionKpi.room_type room_type_enum = InteractionKpi.room_type.values()[room_type];   	
+    	return new InteractionKpi(interaction_type_enum, session_guid, user_id, interaction_value, user_type_enum, room_type_enum);
     }
     
 	public static void main(String[] args) {
 		SpringApplication.run(WhisbikpispringApplication.class, args);
 	}
 }
+
